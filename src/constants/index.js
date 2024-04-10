0e27cfd93ff6cd42dc6604f21a5835fcecdf1632
Deepak <akelladeepak@gmail.com>
2024-04-10 17:24:05 -0400

    Added images to "Popular Recipes"(HOME PAGE DONE BOYSSS)

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,27 +32,27 @@ export const statistics = [
 export const products = [
     {
         imgURL: shoe4,
-        name: "Nike Air Jordan-01",
-        price: "$200.20",
-        rating: "4.5",
+        name: "Baked Mac and Cheese",
+        price: "View Here",
+        rating: "4.6",
     },
     {
         imgURL: shoe5,
-        name: "Nike Air Jordan-10",
-        price: "$210.20",
-        rating: "4.5",
+        name: "Pasta Primavera",
+        price: "View Here",
+        rating: "4.8",
     },
     {
         imgURL: shoe6,
-        name: "Nike Air Jordan-100",
-        price: "$220.20",
-        rating: "4.5",
+        name: "Egg Fried Rice",
+        price: "View Here",
+        rating: "4.7",
     },
     {
         imgURL: shoe7,
-        name: "Nike Air Jordan-001",
-        price: "$230.20",
-        rating: "4.5",
+        name: "Smoothies",
+        price: "View Here",
+        rating: "5.0",
     },
 ];
 
