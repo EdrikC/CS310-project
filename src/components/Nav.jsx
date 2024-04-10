55e6f538da9f177093859ede5d6935da0846a7ea
edrikreyes <edrik.reyes9@gmail.com>
2024-04-10 13:45:00 -0400

    Sticky navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import { logo } from "../assets/images";
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full bg-black text-white'>
+    <header className='padding-x py-8 absolute z-10 w-full bg-black text-white sticky top-0'>
         <nav className='flex justify-between items-center max-container'>
             <a href='#home'><img src={logo} alt="logo" className='min-h-min w-64'/></a>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
